7859e04cf3688ac425299edff0f8e3e982bac0e5
ZengChin <2272330443@qq.com>
2024-10-27 17:48:56 +0800

    Update index.js

--- JS/index.js
+++ JS/index.js
@@ -7,7 +7,7 @@ const front_web = document.querySelector(".front_web")
 
 // console.log(front_web.children[2].opacity);
 
-front_web.children[3].addEventListener("click",function(){
+front_web.children[4].addEventListener("click",function(){
         front_web.classList.add("animation-dying")
         // front_web.classList.remove("animation-dying")
         this.classList.add("animation-trans")
